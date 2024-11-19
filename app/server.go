1016ea4a333e48d247280557e078fe1cda02b3e0
Sonu Kumar <skbhati199@gmail.com>
2024-11-19 11:30:35 +0530

    test cd

--- app/server.go
+++ app/server.go
@@ -53,12 +53,14 @@ func handleConnection(conn net.Conn) {
 			} else {
 				response = "HTTP/1.1 404 Not found\r\n\r\n"
 			}
-		} else if m == "POST" && p[0:7] == "/files/" {
-			content := strings.Trim(r[len(r)-1], "\x00")
-			dir := os.Args[2]
-			_ = os.WriteFile(path.join(dir, p[7:]), []byte(content), 0644)
-			response = "HTTP/1.1 201 Created\r\n\r\n"
-		}
+		} 
+	} else if m == "POST" && p[0:7] == "/files/" {
+		content := strings.Trim(r[len(r)-1], "\x00")
+		dir := os.Args[2]
+
+		// _ = os.WriteFile(path.join(dir, p[7:]), []byte(content), 0644)
+		_ = os.WriteFile(path.Join(dir, p[7:]), []byte(content), 0644)
+		response = "HTTP/1.1 201 Created\r\n\r\n"
 	} else {
 		response = "HTTP/1.1 404 Not Found\r\n\r\n"
 	}
